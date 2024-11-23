7362dccd2062e0bb51c10f3c255c93570912a686
trishikesh <tanya.24.rishikesh@gmail.com>
2024-11-23 18:49:19 +0530

    upates

--- app.js
+++
@@ -1,7 +0,0 @@
-const startAuthServer = require('./authentication.js');
-const startBatteryServer = require('./battery.js');
-
-const mongoUri = "mongodb+srv://trial1:t1@trial-01.y8cbq.mongodb.net/";
-
-startAuthServer(5000, mongoUri);
-startBatteryServer(5001, mongoUri);
